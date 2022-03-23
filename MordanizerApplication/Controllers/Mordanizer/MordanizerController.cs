efc8c340bbee6989a1eed1c1e8f293ba906a6669
Atibur Rahman <atibur@asabd.org>
2022-03-23 14:29:53 +0600

    fresh upload

---
+++ MordanizerApplication/Controllers/Mordanizer/MordanizerController.cs
@@ -0,0 +1,105 @@
+﻿using Aspose.Cells;
+using OfficeOpenXml;
+using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace MordanizerApplication.Controllers.Mordanizer
+{
+    public class MordanizerController : Controller
+    {
+        // GET: Mordanizer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ImportFromExcel(HttpPostedFileBase postedFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (postedFile != null && postedFile.ContentLength > (1024 * 1024 * 50))  // 50MB limit  
+                {
+                    ModelState.AddModelError("postedFile", "Your file is to large. Maximum size allowed is 50MB !");
+                }
+
+                else
+                {
+                    string filePath = string.Empty;
+                    string path = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    filePath = path + Path.GetFileName(postedFile.FileName);
+                    string extension = Path.GetExtension(postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+
+                    string conString = string.Empty;
+                    switch (extension)
+                    {
+                        case ".xls": //For Excel 97-03.  
+                            conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                            break;
+                        case ".xlsx": //For Excel 07 and above.  
+                            conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
+                            break;
+                    }
+
+                    try
+                    {
+                        DataTable dt = new DataTable();
+                        using (var package = new ExcelPackage(postedFile.InputStream))
+                        {
+                            
+                            Workbook workbook = new Workbook(postedFile.InputStream);
+                            Worksheet worksheet = workbook.Worksheets[0];
+                            //worksheet
+                            dt = worksheet.Cells.ExportDataTable(0, 0, worksheet.Cells.MaxDataRow + 1, worksheet.Cells.MaxDataColumn + 1, true);
+                       
+                        }
+
+                        string[] columnNames = dt.Columns.Cast<DataColumn>()
+                                                .Select(x => x.ColumnName)
+                                                .ToArray();
+                        GridView dataGridView1 = new GridView();
+                        dataGridView1.DataSource = columnNames;
+                        dataGridView1.DataBind();
+                        //conString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                        //using (SqlConnection con = new SqlConnection(conString))
+                        //{
+                        //    using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                        //    {
+                        //        //Set the database table name.  
+                        //        sqlBulkCopy.DestinationTableName = "InsuranceCertificate";
+                        //        con.Open();
+                        //        sqlBulkCopy.WriteToServer(dt);
+                        //        con.Close();
+                        //        return Json("File uploaded successfully");
+                        //    }
+                        //}
+                    }
+
+                    //catch (Exception ex)  
+                    //{  
+                    //    throw ex;  
+                    //}  
+                    catch (Exception e)
+                    {
+                        return Json("error" + e.Message);
+                    }
+                    //return RedirectToAction("Index");  
+                }
+            }
+            //return View(postedFile);  
+            return Json("no files were selected !");
+        }
+    }
+}
